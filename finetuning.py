3199edebffb00a9083ee6c46f593cc0a3aae0b49
gabrielbarcik <34548900+gabrielbarcik@users.noreply.github.com>
2018-02-07 16:18:40 +0100

    Add files via upload

--- finetuning.py
+++ finetuning.py
@@ -1,14 +1,15 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 from time import time
 import keras
-from keras.callbacks import TensorBoard
+#from keras.callbacks import TensorBoard
 from keras.models import model_from_json
 from keras.models import Sequential, Model
 from keras.optimizers import SGD
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Dropout, Flatten, merge, Reshape, Activation
 from sklearn.metrics import log_loss
+import splitdata as sp
+from sklearn.metrics import confusion_matrix
+import pandas as pd
 
 # Constructing vgg16 net
 def vgg16_base_model(img_rows, img_cols, channel=1, num_classes=None):
@@ -21,11 +22,15 @@ def vgg16_base_model(img_rows, img_cols, channel=1, num_classes=None):
     
     #### KEY PART ####
     # Create a Top Model
-    top_model = Sequential()
-    top_model.add(Flatten(input_shape=model.output_shape[1:]))
-    top_model.add(Dense(128, activation='relu'))
-    top_model.add(Dropout(0.15))
-    top_model.add(Dense(4, activation='sigmoid'))
+    # top_model = Sequential()
+    # top_model.add(Flatten(input_shape=model.output_shape[1:]))
+    # top_model.add(Dense(128, activation='relu'))
+    # top_model.add(Dropout(0.15))
+    # top_model.add(Dense(4, activation='sigmoid'))
+
+    # top_model = Sequential()
+    # top_model.add(Flatten(input_shape=model.output_shape[1:]))
+    # top_model.add(Dense(4, activation='softmax'))
     
     ##################
 
@@ -33,32 +38,36 @@ def vgg16_base_model(img_rows, img_cols, channel=1, num_classes=None):
     new_model = Sequential()
     for l in model.layers:
         new_model.add(l)
-        
-    # Lock the top conv layers
+    
+    # Pop the last layer
+    new_model.layers.pop()
+    # new_model.outputs = [new_model.layers[-1].output]
+    # new_model.layers[-1].outbound_nodes = []
+
     for layer in new_model.layers:
-        layer.trainable = False    
+        layer.trainable = False 
+
+
+    # Using just one final layer: number of parameters 53252
+
+    new_model.add(Flatten(input_shape=new_model.output_shape[1:]))
+    new_model.add(Dense(4, activation='softmax'))
+
+    # Lock the top conv layers
+    # for layer in new_model.layers[:12]:
+    #     layer.trainable = False    
         
     # Concatenate the two models
-    new_model.add(top_model)
+    #new_model.add(top_model)
         
     # Compile Model
     sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     new_model.compile(loss='categorical_crossentropy',
-                      optimizer='adam',
+                      optimizer='sgd',
                       metrics=['accuracy'])
     new_model.summary()
-    return new_model
+    return new_model   
 
-# Split Data X, y in train set and test set
-def split_data(X, y, test_size):
-    onehotencoder = preprocessing.LabelEncoder()
-    onehotencoder.fit(y)
-    y = keras.utils.to_categorical(onehotencoder.transform(y), 4)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, 
-                                                        random_state=42, stratify=y)                                                       
-    return X_train, X_test, y_train, y_test
-    
-    
 if __name__ == '__main__':
     
     # Set parameters and constants
@@ -66,30 +75,16 @@ if __name__ == '__main__':
     channel = 1
     num_classes = 4
     batch_size = 32
-    nb_epoch = 5
+    nb_epoch = 1
     test_size = 0.20  
-        
-    # Load data
-    spectrograms = np.load('data.npy')
-    labels = np.load('labels.npy')
-
-    # Preprossing DATA
-    y = labels[:, 0]
-    X = spectrograms
-    X = np.reshape(X, (550, 96, 862))
-    for i, matrix in enumerate(X):
-        X[i] = X[i] - np.mean(matrix)
-        X[i] /= np.std(matrix)
-    X = np.reshape(X, (550, 64, 431, 3))
-
-    # Spliting the dataset in traning set and test set
-    X_train, X_test, y_train, y_test = split_data(X, y, test_size)
     
+    X_train, X_test, y_train, y_test, classes = sp.split_data(homo=True)
+        
     # Constructing a vgg16 net    
     model = vgg16_base_model(img_rows, img_cols, channel, num_classes=num_classes)
     
     # Definition of the tensorboard
-    tensorboard = TensorBoard(log_dir="logs/{}".format(time()), histogram_freq=1, write_graph=True, write_images=True)
+    #tensorboard = TensorBoard(log_dir="logs/{}".format(time()), histogram_freq=1, write_graph=True, write_images=True)
 
     # Fit Model
     model.fit(X_train, y_train,
@@ -97,8 +92,8 @@ if __name__ == '__main__':
               nb_epoch=nb_epoch,
               shuffle=True,
               verbose=1,
-              validation_data=(X_test, y_test),
-              callbacks=[tensorboard]
+              validation_data=(X_test, y_test)
+             # callbacks=[tensorboard]
               )
 
     # Predict Model
@@ -106,16 +101,22 @@ if __name__ == '__main__':
     
     score = log_loss(y_test, predictions_valid)
     print(score)
+    
+    predictions_valid = np.asarray(predictions_valid)
+    predictions_valid = np.argmax(predictions_valid, axis=1)
+    y_test = np.argmax(y_test, axis=1)
 
+    confmatrix = confusion_matrix(predictions_valid, y_test)
+    
+    np.save('confusion_matrix.npy', confmatrix)
+    
     # SAVING THE MODEL
 
     # serialize model to JSON
     model_json = model.to_json()
-    with open("model.json", "w") as json_file:
+    with open("model_homo.json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    model.save_weights("model.h5")
+    model.save_weights("model_homo.h5")
     print("Saved model to disk")
-
-    
     
